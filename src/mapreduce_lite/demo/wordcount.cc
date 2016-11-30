efcf6cde13710df1e7103a9ced35a187825acc75
Michael <lwhaymail@yahoo.com>
2016-11-29 23:06:01 -0800

    Format the wordcount.

--- src/mapreduce_lite/demo/wordcount.cc
+++ src/mapreduce_lite/demo/wordcount.cc
@@ -165,79 +165,79 @@ using mapreduce_lite::ReduceInputIterator;
 
 
 class WordCountMapper : public Mapper {
-public:
-  void Map(const std::string& key, const std::string& value) {
-    std::vector<std::string> words;
-    SplitStringUsing(value, " ", &words);
-    for (int i = 0; i < words.size(); ++i) {
-      Output(words[i], "1");
+  public:
+    void Map(const std::string& key, const std::string& value) {
+      std::vector<std::string> words;
+      SplitStringUsing(value, " ", &words);
+      for (int i = 0; i < words.size(); ++i) {
+        Output(words[i], "1");
+      }
     }
-  }
 };
 REGISTER_MAPPER(WordCountMapper);
 
 
 class WordCountMapperWithCombiner : public Mapper {
-public:
-  void Map(const std::string& key, const std::string& value) {
-    std::vector<std::string> words;
-    SplitStringUsing(value, " ", &words);
-    for (int i = 0; i < words.size(); ++i) {
-      ++combined_results[words[i]];
+  public:
+    void Map(const std::string& key, const std::string& value) {
+      std::vector<std::string> words;
+      SplitStringUsing(value, " ", &words);
+      for (int i = 0; i < words.size(); ++i) {
+        ++combined_results[words[i]];
+      }
     }
-  }
 
-  void Flush() {
-    for (map<string, int>::const_iterator i = combined_results.begin();
-         i != combined_results.end(); ++i) {
-      ostringstream formater;
-      formater << i->second;
-      Output(i->first, formater.str());
+    void Flush() {
+      for (map<string, int>::const_iterator i = combined_results.begin();
+           i != combined_results.end(); ++i) {
+        ostringstream formater;
+        formater << i->second;
+        Output(i->first, formater.str());
+      }
+      // NOTE: for the correctness when we enable periodic-flush, we
+      // must clear the intermediate data after flush it.
+      combined_results.clear();
     }
-    // NOTE: for the correctness when we enable periodic-flush, we
-    // must clear the intermediate data after flush it.
-    combined_results.clear();
-  }
-private:
-  map<string, int> combined_results;
+  private:
+    map<string, int> combined_results;
 };
 REGISTER_MAPPER(WordCountMapperWithCombiner);
 
 
 
 class WordCountReducer : public IncrementalReducer {
-public:
-  void Start() {
-    Output("", "Following lines are reduce outputs:");
-  }
+  public:
+    void Start() {
+      Output("", "Following lines are reduce outputs:");
+    }
 
-  void* BeginReduce(const std::string& key, const std::string& value) {
-    istringstream parser(value);
-    int* r = new int;
-    parser >> *r;
-    return r;
-  }
+    void* BeginReduce(const std::string& key, const std::string& value) {
+      istringstream parser(value);
+      int* r = new int;
+      parser >> *r;
+      return r;
+    }
 
-  void PartialReduce(const std::string& key,
-                     const std::string& value,
-                     void* partial_result) {
-    istringstream parser(value);
-    int count = 0;
-    parser >> count;
-    *static_cast<int*>(partial_result) += count;
-  }
+    void PartialReduce(const std::string& key,
+                       const std::string& value,
+                       void* partial_result) {
+      istringstream parser(value);
+      int count = 0;
+      parser >> count;
+      *static_cast<int*>(partial_result) += count;
+    }
 
-  void EndReduce(const string& key, void* partial_result) {
-    int* p = static_cast<int*>(partial_result);
-    ostringstream formater;
-    formater << key << " " << *p; // If output format is Text, only value will
-    Output(key, formater.str());  // be written.  So we write key into value.
-    delete p;
-  }
+    void EndReduce(const string& key, void* partial_result) {
+      int* p = static_cast<int*>(partial_result);
+      ostringstream formater;
+      formater << key << " " << *p; // If output format is Text, only value will
+      Output(key, formater.str());  // be written.  So we write key into value.
+      delete p;
+    }
 
-  void Flush() {
-    Output("", "Above lines are reduce outputs.");
-  }
+    void Flush() {
+      Output("", "Above lines are reduce outputs.");
+    }
 };
 REGISTER_INCREMENTAL_REDUCER(WordCountReducer);
 
@@ -247,31 +247,31 @@ REGISTER_INCREMENTAL_REDUCER(WordCountReducer);
 // using REGISTER_MR_REDUCER; whereas in incremental reduction mode, reducers
 // are derived from MRML_Reducer and registered using REGISTER_REDUCER.
 class WordCountBatchReducer : public BatchReducer {
-public:
-  void Start() {
-    //Output("", "Following lines are MR reduce outputs:");
-  }
-
-  void Reduce(const string& key, ReduceInputIterator* values) {
-    int sum = 0;
-    LOG(INFO) << "key:[" << key << "]";
-    for (; !values->Done(); values->Next()) {
-      //LOG(INFO) << "value:[" << values->value() << "]";
-      istringstream parser(values->value());
-      int count = 0;
-      parser >> count;
-      sum += count;
+  public:
+    void Start() {
+      //Output("", "Following lines are MR reduce outputs:");
     }
-    ostringstream formater;
-    formater << key << " " << sum;
-    Output(key, formater.str());
 
-    // Example of writing logs, choices are LOG(INFO), LOG(WARNING), LOG(FATAL)
-    //LOG(INFO) << "Reduce for key : " << key;
-  }
+    void Reduce(const string& key, ReduceInputIterator* values) {
+      int sum = 0;
+      LOG(INFO) << "key:[" << key << "]";
+      for (; !values->Done(); values->Next()) {
+        //LOG(INFO) << "value:[" << values->value() << "]";
+        istringstream parser(values->value());
+        int count = 0;
+        parser >> count;
+        sum += count;
+      }
+      ostringstream formater;
+      formater << key << " " << sum;
+      Output(key, formater.str());
 
-  void Flush() {
-    //Output("", "Above lines are MR reduce outputs.");
-  }
+      // Example of writing logs, choices are LOG(INFO), LOG(WARNING), LOG(FATAL)
+      //LOG(INFO) << "Reduce for key : " << key;
+    }
+
+    void Flush() {
+      //Output("", "Above lines are MR reduce outputs.");
+    }
 };
 REGISTER_BATCH_REDUCER(WordCountBatchReducer);
